0910df750f3551350dabc955ea4e2302a0cae282
Jeppe Klitgaard <jeppe@dapj.dk>
2015-07-10 23:03:53 +0200

    Docstrings and removal of unused imports

--- leapcast/utils.py
+++ leapcast/utils.py
@@ -1,35 +1,40 @@
-from __future__ import unicode_literals
-from six.moves.socketserver import ThreadingUDPServer
-import logging
+"""
+Provides utility functions and classes used by Leapcast.
+"""
+
 import socket
 import struct
-import operator
-import contextlib
+from contextlib import closing as ctx_closing
 from textwrap import dedent
-import threading
 from netifaces import interfaces, ifaddresses, AF_INET
 
 from tornado.template import Template
-
-from .mixins import ControlMixin
 
 SIOCGIFADDR = 0x8915
 
 
 def render(template):
+    """
+    Renders a template using Tornado.
+    """
     return Template(dedent(template))
 
 
 def get_interface_address(if_name):
+    """
+    Returns the address of an interface.
+    """
     import fcntl  # late import as this is only supported on Unix platforms.
 
-    with contextlib.closing(
-            socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as s:
+    with ctx_closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as s:
         return fcntl.ioctl(s.fileno(), SIOCGIFADDR,
                            struct.pack(b'256s', if_name[:15]))[20:24]
 
 
 def get_remote_ip(address):
+    """
+    Returns the remote ip address.
+    """
     # Create a socket to determine what address the client should
     # use
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -40,6 +45,9 @@ def get_remote_ip(address):
 
 
 def get_own_ip():
+    """
+    Returns the best guess of what a good ip of this machine is on the network.
+    """
     addresses = set()
     for ifname in interfaces():
         for iaddr in ifaddresses(ifname).setdefault(AF_INET, [{"addr": None}]):
